6405536b934351bf345d2f9e4661d86d0e6578ed
dinhnn <nhudinh2103@gmail.com>
2025-12-23 17:28:12 +0700

    misc: update Sql logging config for correctly log sql

--- src/main/java/com/tymex/payment/config/SqlLoggingConfig.java
+++ src/main/java/com/tymex/payment/config/SqlLoggingConfig.java
@@ -31,21 +31,34 @@ public class SqlLoggingConfig {
         
         // Configure Hibernate SQL statement logging
         ch.qos.logback.classic.Logger hibernateSqlLogger = loggerContext.getLogger("org.hibernate.SQL");
-        hibernateSqlLogger.setLevel(sqlLoggingEnabled ? Level.DEBUG : Level.INFO);
+        if (sqlLoggingEnabled) {
+            hibernateSqlLogger.setLevel(Level.DEBUG);
+        } else {
+            hibernateSqlLogger.setLevel(Level.INFO);
+        }
         
         // Configure Hibernate SQL parameter binding logging (shows actual parameter values)
         ch.qos.logback.classic.Logger hibernateParamsLogger = loggerContext.getLogger("org.hibernate.orm.jdbc.bind");
-        hibernateParamsLogger.setLevel(sqlLoggingEnabled ? Level.TRACE : Level.INFO);
+        if (sqlLoggingEnabled) {
+            hibernateParamsLogger.setLevel(Level.TRACE);
+        } else {
+            hibernateParamsLogger.setLevel(Level.INFO);
+        }
         
         // Also set for older Hibernate versions (5.x compatibility)
         ch.qos.logback.classic.Logger hibernateTypeLogger = loggerContext.getLogger("org.hibernate.type.descriptor.sql.BasicBinder");
-        hibernateTypeLogger.setLevel(sqlLoggingEnabled ? Level.TRACE : Level.INFO);
+        if (sqlLoggingEnabled) {
+            hibernateTypeLogger.setLevel(Level.TRACE);
+        } else {
+            hibernateTypeLogger.setLevel(Level.INFO);
+        }
         
         // Suppress SQL exception logging for expected constraint violations
         // DataIntegrityViolationException is caught and handled as part of normal flow (idempotency check)
-        // Setting to OFF completely suppresses these expected constraint violation logs
+        // Setting to OFF suppresses duplicate key error logs, but INSERT SQL statements from org.hibernate.SQL
+        // will still be visible when sql-logging is enabled
         ch.qos.logback.classic.Logger sqlExceptionLogger = loggerContext.getLogger("org.hibernate.engine.jdbc.spi.SqlExceptionHelper");
-        sqlExceptionLogger.setLevel(Level.OFF); // Completely suppress expected constraint violation logs
+        sqlExceptionLogger.setLevel(Level.OFF); // Suppress all SQL exception logs (including duplicate key errors)
         
         if (sqlLoggingEnabled) {
             log.info("SQL logging enabled - All SQL statements and parameters will be logged");
